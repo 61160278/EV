cb77ff7ff68a8ab0654beb9ae8914b2741ea6aea
LapatradaPuttamongkol <70052206+LapatradaPuttamongkol@users.noreply.github.com>
2021-04-17 23:47:56 +0700

    Update v_detail_quota.php

--- application/views/consent/ev_quota/v_detail_quota.php
+++ application/views/consent/ev_quota/v_detail_quota.php
@@ -59,26 +59,26 @@ function check_quota_plan(){
 	}//for
 	
 
-}
+// }
 
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-canvas.height = canvas.width;
-ctx.transform(1, 0, 0, -1, 0, canvas.height)
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
+// ctx.fillStyle = "#FF0000";
+// canvas.height = canvas.width;
+// ctx.transform(1, 0, 0, -1, 0, canvas.height)
 
-let xMax = canvas.height;
-let slope = 1.2;
-let intercept = 70;
+// let xMax = canvas.height;
+// let slope = 1.2;
+// let intercept = 70;
 
-ctx.moveTo(0, intercept);
-ctx.lineTo(xMax, f(xMax));
-ctx.strokeStyle = "black";
-ctx.stroke();
+// ctx.moveTo(0, intercept);
+// ctx.lineTo(xMax, f(xMax));
+// ctx.strokeStyle = "black";
+// ctx.stroke();
 
-function f(x) {
-  return x * slope + intercept;
-}
+// function f(x) {
+//   return x * slope + intercept;
+// }
 </script>
 
 <div class="col-md-12">
