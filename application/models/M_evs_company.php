4882b647c3a3a5fb3a51f4525a91b6008153d229
Aofltyphoto <aofly2542@gmail.com>
2021-04-21 00:08:46 +0700

    Create M_evs_company.php

---
+++ application/models/M_evs_company.php
@@ -0,0 +1,55 @@
+<?php
+/*
+* M_evs_company
+* Set Company Management
+* @author Piyasak Srijan
+* @Create Date 2564-04-20
+*/ 
+
+?>
+<?php
+include_once("Da_evs_company.php");
+
+/*
+* M_evs_company
+* set Company management
+* @author Piyasak Srijan
+* @Create Date 2564-04-20
+*/
+
+class M_evs_company extends Da_evs_company {
+
+
+	/*
+	* get_all
+	* Get All Company Level from database
+	* @input  -
+	* @output company all
+	* @author Piyasak Srijan
+	* @Create Date 2564-04-20
+	*/
+	function get_all(){	
+		$sql = "SELECT * 
+				FROM dbmc.company" ;
+		$query = $this->db->query($sql);
+		return $query;
+	}//get_all
+
+	
+	/*
+	* get_company
+	* Get All Company Level from database
+	* @input  -
+	* @output company all
+	* @author Piyasak Srijan
+	* @Create Date 2564-04-20
+	*/
+	function get_company() {	
+		$sql = "SELECT * 
+				FROM dbmc.company
+				WHERE company.Company_ID=?" ;
+		$query = $this->db->query($sql, array($this->cpn_id));
+		return $query;
+	}//get_position
+}
+?>
