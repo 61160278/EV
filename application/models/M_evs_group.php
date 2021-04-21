bd9cd501ae3fb34141a5c36f890d9fc0f46da652
61160287 <70052050+61160287@users.noreply.github.com>
2021-04-21 09:45:41 +0700

    Update M_evs_group.php

--- application/models/M_evs_group.php
+++ application/models/M_evs_group.php
@@ -44,7 +44,6 @@ class M_evs_group extends Da_evs_group {
 	
 	}//get_all_com
 
-
 	function get_name_emp_by_IDemp(){	
 		$sql = "SELECT *
 				FROM dbmc.employee
@@ -53,7 +52,6 @@ class M_evs_group extends Da_evs_group {
 		return $query->result();
 
 	}//get_all_com  INNER JOIN dbmc.employee as emp ON emp.Emp_ID = evg.gru_head_dept
-	
 	
 	function connect(){
 		$sql = "SELECT *
