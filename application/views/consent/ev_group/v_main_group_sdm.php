ae0306a6b51d73aa370b9288e853f5541d2bd7cd
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-04-22 14:33:48 +0700

    Update v_main_group_sdm.php

--- application/views/consent/ev_group/v_main_group_sdm.php
+++ application/views/consent/ev_group/v_main_group_sdm.php
@@ -153,8 +153,8 @@ function get_Emp() {
 
 function warning() {
 
-      alert("Please input data !");
-
+      // alert("Please input data !");
+      $('#warning').modal('show');
 
 }
 
@@ -642,3 +642,60 @@ function check_data_edt(check) {
       <!-- modal-dialog -->
 </div>
 <!-- End Modal Add-->
+
+
+  <!-- Modal Warning -->
+                                                <div class="modal fade" id="warning"
+                                                            tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+                                                            aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                  <div class="modal-content">
+                                                                        <div class="modal-header"
+                                                                              style="background-color:#FF9800;">
+                                                                              <button type="button" class="close"
+                                                                                    data-dismiss="modal"
+                                                                                    aria-hidden="true">
+                                                                                    <font color="White"><b>&times;</b>
+                                                                                    </font>
+                                                                              </button>
+                                                                              <h2 class="modal-title"><b>
+                                    <font color="white">Warning</font>
+                              </b></h2>
+                                                                        </div>
+                                                                        <!-- Modal header -->
+
+                                                                        <div class="modal-body">
+                                                                              <div class="form-horizontal">
+                                                                                    <div class="form-group"
+                                                                                          align="center">
+                                                                                          <div class="col-sm-12">
+                                                                                                <label for="focusedinput"
+                                                                                                      class="control-label"
+                                                                                                      style="font-family:'Courier New'"
+                                                                                                      align="center">
+                                                                                                      <font size="5px">
+                                                                                                      Please fill in the correct information.</font>
+                                                                                                </label>
+
+                                                                                          </div>
+                                                                                    </div>
+                                                                              </div>
+                                                                              <!-- form-horizontal -->
+                                                                        </div>
+                                                                        <!-- Modal body -->
+
+                                                                        <div class="modal-footer">
+                                                                              <div class="btn-group pull-right">
+                                                                                    <button type="button"
+                                                                                          class="btn btn-inverse"
+                                                                                          data-dismiss="modal">Yes</button>
+                                                                              </div>
+                                                                              
+                                                                        </div>
+                                                                        <!-- Modal footer -->
+                                                                  </div>
+                                                                  <!-- modal-content -->
+                                                            </div>
+                                                            <!-- modal-dialog -->
+                                                      </div>
+                                                      <!-- End Modal Warning -->
