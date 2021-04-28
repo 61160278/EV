020ac23638a8583c5a6f51f1da3621eee134163e
piyasak61160185 <66992966+piyasak18013@users.noreply.github.com>
2021-04-28 15:37:58 +0700

    Update v_add_quota.php

--- application/views/consent/ev_quota/v_add_quota.php
+++ application/views/consent/ev_quota/v_add_quota.php
@@ -58,9 +58,9 @@ function check_quota() {
         // if 
         if (value_quota > 100) {
             $("#show_quota").css("color", "red");
-        
-            alert("The sum of qouta values more than 100!");
-    
+
+            add_alert();
+
         } else {
             $("#show_quota").css("color", "#000000");
         }
@@ -70,6 +70,11 @@ function check_quota() {
     // for i
 
 }
+
+function add_alert() {
+    $('#warning').modal('show');
+}
+
 
 function show_qouta() {
 
@@ -92,7 +97,7 @@ function show_qouta() {
 
     } //ค่าที่รับจากตารางที่เปลี่ยนจากstring เป็น int
     console.log(dataQuota);
-   
+
     //<block:setup:1>
     const labels = [
         'S',
@@ -130,7 +135,7 @@ function show_qouta() {
     var ctx = document.getElementById('myChart').getContext('2d');
 
     var myChart = new Chart(ctx, config);
-   
+
 
 } //showChart
 </script>
@@ -144,111 +149,156 @@ function show_qouta() {
                 data-action-collapse='{"target": ".panel-body, .panel-footer"}'>
             </div>
         </div>
-        <div class="panel-body" style="">  
-        <form>
-            <div class="row">
-                <div class="form-group">
-                    <div class="col-md-3">
+        <div class="panel-body" style="">
+            <form>
+                <div class="row">
+                    <div class="form-group">
+                        <div class="col-md-3">
+                        </div>
+                        <div class="col-md-3">
+                            <select class="form-control text" id="">
+                                <option value="yearEndBonus">Quota</option>
+                                <option value="yearEndBonus">Year End Bonus</option>
+                                <option value="salaryIncrement">Salary Increment</option>
+                            </select>
+                        </div>
+                        <div class="col-md-3">
+                            <select class="form-control text" id="">
+                                <option value="yearEndBonus">Position Of Quota</option>
+                                <option value="yearEndBonus">Team Associate above</option>
+                                <option value="salaryIncrement">Operational Associate</option>
+                            </select>
+                        </div>
+
                     </div>
-                    <div class="col-md-3">
-                        <select class="form-control text" id="">
-                            <option value="yearEndBonus">Quota</option>
-                            <option value="yearEndBonus">Year End Bonus</option>
-                            <option value="salaryIncrement">Salary Increment</option>
-                        </select>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-2">
                     </div>
-                    <div class="col-md-3">
-                        <select class="form-control text" id="">
-                            <option value="yearEndBonus">Position Of Quota</option>
-                            <option value="yearEndBonus">Team Associate above</option>
-                            <option value="salaryIncrement">Operational Associate</option>
-                        </select>
+                    <div class="col-md-8">
+                        <table class="table table-hover m-n orange">
+                            <thead>
+                                <tr>
+                                    <th>Grade</th>
+                                    <th>S</th>
+                                    <th>A</th>
+                                    <th>B</th>
+                                    <th>B-</th>
+                                    <th>C</th>
+                                    <th>D</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="orange2">
+                                    <td>Quota</td>
+                                    <td>
+                                        <input type="text" class="form-control" id="quota1" onchange="check_quota()">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" id="quota2" onchange="check_quota()">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" id="quota3" onchange="check_quota()">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" id="quota4" onchange="check_quota()">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" id="quota5" onchange="check_quota()">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" id="quota6" onchange="check_quota()">
+                                    </td>
+                                    <td id="show_quota"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+
                     </div>
 
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-md-2">
-                </div>
-                <div class="col-md-8">
-                    <table class="table table-hover m-n orange">
-                        <thead>
-                            <tr>
-                                <th>Grade</th>
-                                <th>S</th>
-                                <th>A</th>
-                                <th>B</th>
-                                <th>B-</th>
-                                <th>C</th>
-                                <th>D</th>
-                                <th>Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="orange2">
-                                <td>Quota</td>
-                                <td>
-                                    <input type="text" class="form-control" id="quota1" onchange="check_quota()">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" id="quota2" onchange="check_quota()">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" id="quota3" onchange="check_quota()">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" id="quota4" onchange="check_quota()">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" id="quota5" onchange="check_quota()">
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" id="quota6" onchange="check_quota()">
-                                </td>
-                                <td id="show_quota"></td>
-                            </tr>
-                        </tbody>
-                    </table>
 
                 </div>
+                <br>
 
+                <div class="row">
+                    <div class="col-md-offset-9">
 
-            </div>
-            <br>
-
-            <div class="row">
-                <div class="col-md-offset-9">
-              
-                    <buuton class="btn btn-success" type = "submit" onclick="show_qouta()">Submit</buuton>
-                    <!-- <buuton class="btn btn-success" type = "reset" ></buuton> -->
-                </from>
+                        <buuton class="btn btn-success" type="submit" onclick="show_qouta()">Submit</buuton>
+                        <!-- <buuton class="btn btn-success" type = "reset" ></buuton> -->
+                        </from>
+                    </div>
                 </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-md-2">
-                </div>
-                <div class="col-md-8">
-                    <div class="panel panel-midnightblue" data-widget='{"draggable": "false"}'>
-                        <div class="panel-heading">
-                            <h2>
-                                <font size="5px"><b>Quota</b></font>
-                            </h2>
-                            <div class="panel-ctrls" data-actions-container=""
-                                data-action-collapse='{"target": ".panel-body"}'>
+                <br>
+                <div class="row">
+                    <div class="col-md-2">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="panel panel-midnightblue" data-widget='{"draggable": "false"}'>
+                            <div class="panel-heading">
+                                <h2>
+                                    <font size="5px"><b>Quota</b></font>
+                                </h2>
+                                <div class="panel-ctrls" data-actions-container=""
+                                    data-action-collapse='{"target": ".panel-body"}'>
+                                </div>
+                            </div>
+                            <div class="panel-body">
+                                <canvas id="myChart" width="1000" height="450" style="position: relative;"></canvas>
+
                             </div>
                         </div>
-                        <div class="panel-body">
-                            <canvas id="myChart" width="1000" height="450" style="position: relative;"></canvas>
-
-                        </div>
                     </div>
                 </div>
-            </div>
-            <button type="button" class="btn btn-inverse pull-left" data-dismiss="modal">CANCEL</button>
-            <button type="button" class="btn btn-social pull-right" style="background-color:#0000CD;">SAVE</button>
+                <button type="button" class="btn btn-inverse pull-left" data-dismiss="modal">CANCEL</button>
+                <button type="button" class="btn btn-social pull-right" style="background-color:#0000CD;">SAVE</button>
         </div>
+        <!-- Modal Warning -->
+        <div class="modal fade" id="warning" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header" style="background-color:#FF9800;">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                            <font color="White"><b>&times;</b>
+                            </font>
+                        </button>
+                        <h2 class="modal-title"><b>
+                                <font color="white">Warning</font>
+                            </b></h2>
+                    </div>
+                    <!-- Modal header -->
+
+                    <div class="modal-body">
+                        <div class="form-horizontal">
+                            <div class="form-group" align="center">
+                                <div class="col-sm-12">
+                                    <label for="focusedinput" class="control-label" style="font-family:'Courier New'"
+                                        align="center">
+                                        <font size="3px">
+                                            Value is more than 100</font>
+                                    </label>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- form-horizontal -->
+                    </div>
+                    <!-- Modal body -->
+
+                    <div class="modal-footer">
+                        <div class="btn-group pull-right">
+                            <button type="button" class="btn btn-success" data-dismiss="modal">Yes</button>
+                        </div>
+
+                    </div>
+                    <!-- Modal footer -->
+                </div>
+                <!-- modal-content -->
+            </div>
+            <!-- modal-dialog -->
+        </div>
+        <!-- End Modal Warning -->
     </div>
-</div>
 </div>
