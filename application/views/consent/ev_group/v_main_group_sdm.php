5b5bb54e61fd2eecc62801834b4535040ab5433c
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-04-30 16:56:06 +0700

    Update v_main_group_sdm.php

--- application/views/consent/ev_group/v_main_group_sdm.php
+++ application/views/consent/ev_group/v_main_group_sdm.php
@@ -535,9 +535,9 @@ function check_data_edt(check) {
                                                 id="grouptext<?php echo $row->gru_id; ?>" name="grouptext"
                                                 placeholder="HR AGM" onkeyup="clear_css(<?php echo $row->gru_id; ?>)">
                                           <label class="col-sm-12 control-label"></label>
-                                          <p id="alert_grouptext_edt<?php echo $row->gru_id; ?>" >
+                                          <!-- <p id="alert_grouptext_edt<?php echo $row->gru_id; ?>" >
                                                 <font color="red"><b>This data already to used! </b></font>
-                                          </p>
+                                          </p> -->
 
                                     </div>
                               </div>
