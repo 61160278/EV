eafec774448efb9a289e9f5835bcfb6d044254ab
61160287 <70052050+61160287@users.noreply.github.com>
2021-05-05 17:02:17 +0700

    Update v_main_group_skd.php

--- application/views/consent/ev_group/v_main_group_skd.php
+++ application/views/consent/ev_group/v_main_group_skd.php
@@ -9,6 +9,20 @@
 */  
 ?>
 
+<head>
+    <style>
+    thead {
+        color: black;
+        font-size: 17px;
+    }
+
+    tbody {
+        color: black;
+        font-size: 14px;
+    }
+    </style>
+</head>
+
 <script>
 function manage_skd(value) {
     if (value == "0") {
@@ -51,6 +65,34 @@ function add_group() {
     window.location.href = "<?php echo base_url();?>/ev_group/Evs_group/select_company_skd";
 }
 // function add_group
+
+function edit_group(gru_id) {
+
+    var grouptext = document.getElementById("group_text" + gru_id).value;
+    var Emp_id = document.getElementById("Emp_id" + gru_id).value;
+    var Showname_modol = document.getElementById("nameEmp" + gru_id).value;
+
+    $.ajax({
+        type: "POST",
+        url: "<?php echo base_url(); ?>/ev_group/Evs_group/save_edit_skd",
+        data: {
+            "group_text": grouptext,
+            "Emp_id": Emp_id,
+            "gru_id": gru_id
+
+        },
+        dataType: "JSON",
+        error: function(status) {
+            console.log(status)
+        }
+        // success function
+
+    });
+
+    window.location.href = "<?php echo base_url();?>/ev_group/Evs_group/select_company_skd";
+
+}
+//function add_group
 
 function delete_data(gru_id) {
     console.log(gru_id);
@@ -164,7 +206,7 @@ function check_add() {
             return false;
         }
         // if-else Showname_modol
-    } else { 
+    } else {
         add_alert();
         return false;
     }
@@ -176,13 +218,35 @@ function check_edit_skd(check) {
     var group = document.getElementById("group_text" + check).value;
     var Emp_id = document.getElementById("Emp_id" + check).value;
     var Showname_modol = document.getElementById("nameEmp" + check).value;
+    var count = 0;
     console.log(group)
     console.log(Emp_id)
     console.log(Showname_modol)
 
     if (group != "" && Emp_id != "") {
         if (Showname_modol != "ไม่มีข้อมูล") {
-            return true;
+            $.get("<?php echo base_url(); ?>/ev_group/Evs_group/get_group_skd ", function(data, status) {
+                var obj = JSON.parse(data); //แปลงค่าข้อมูล JSON
+                obj.forEach((row, index) => { //row =data
+
+                    if (group == row.gru_name) {
+                        count++;
+                        console.log(count)
+                    }
+                    // if-else
+                });
+                // forEach
+                if (count == 0) {
+                    console.log("true")
+
+                    edit_group(check);
+                    return true;
+                } else {
+                    $("#btnedit" + check).attr("disabled", true);
+                    return false;
+                }
+            });
+            // $.get
         } else {
             add_alert();
             return false;
@@ -207,6 +271,12 @@ function manage_data(gru_id) {
     window.location.href = "<?php echo base_url(); ?>/ev_group/Evs_group/select_group_company_skd/" + gru_id;
 }
 // manage_data
+
+function clear_css(gru_id) {
+    $("#alert_grouptext_edt" + gru_id).hide();
+    $("#btnedit" + gru_id).attr("disabled", false);
+}
+//function clear_css
 </script>
 
 <!DOCTYPE html>
@@ -326,7 +396,9 @@ function manage_data(gru_id) {
                     <!-- panel-footer -->
                 </div>
                 <!-- panel-addtable -->
-                <h4 class="text" style="font-family:'Times New Roman'"><font size="5px">Description</font></h4>
+                <h4 class="text" style="font-family:'Times New Roman'">
+                    <font size="5px">Description</font>
+                </h4>
                 <div>
                     <a class="btn btn-danger">
                         <i class="ti ti-trash"></i>
@@ -353,21 +425,6 @@ function manage_data(gru_id) {
 </div>
 <!-- head outside -->
 
-</html>
-
-<head>
-    <style>
-    thead {
-        color: black;
-        font-size: 17px;
-    }
-
-    tbody {
-        color: black;
-        font-size: 14px;
-    }
-    </style>
-</head>
 
 <!-- Modal Add -->
 <div class="modal fade" id="Add" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -437,7 +494,7 @@ function manage_data(gru_id) {
                 <div class="btn-group pull-left">
                     <button type="button" class="btn btn-inverse" data-dismiss="modal">CANCEL</button>
                 </div>
-                <button type="button" class="btn btn-success" id="btnsaveadd" onclick="check_add()">SAVE</button>
+                <button type="button" class="btn btn-success" id="btnsaveadd" onclick="return check_add()">SAVE</button>
             </div>
             <!-- modal-footer -->
         </div>
@@ -461,31 +518,29 @@ function manage_data(gru_id) {
                     <font color="White"><b>&times;</b></font>
                 </button>
                 <h2 class="modal-title" style="font-family:'Georgia'"><b>
-                    <font color="white">Edit SKD Group Data & Head Dept.</font>
-                </b></h2>
+                        <font color="white">Edit SKD Group Data & Head Dept.</font>
+                    </b></h2>
             </div>
             <!-- modal header -->
 
             <div class="modal-body">
-                <form class="form-horizontal" action="<?php echo base_url(); ?>ev_group/Evs_group/save_edit_skd"
-                    method="post" onsubmit="return check_edit_skd('<?php echo $row->gru_id; ?>')">
+                <form class="form-horizontal">
                     <div class="form-group">
                         <label for="focusedinput" class="col-sm-3 control-label">Group Name</label>
                         <div class="col-sm-6">
                             <input type="text" class="form-control" value="<?php echo $row->gru_name; ?>"
-                                id="group_text<?php echo $row->gru_id; ?>" name="group_text" placeholder="HR AGM">
+                                id="group_text<?php echo $row->gru_id; ?>" name="group_text" placeholder="HR AGM"
+                                onkeyup="clear_css(<?php echo $row->gru_id; ?>)">
+                            <label class="col-sm-12 control-label"></label>
                         </div>
                     </div>
                     <!-- Group Name -->
 
-                    <div class="form-group">
-                        <label class="col-sm-1 control-label"></label>
-                        <div class="col-sm-8">
-                            <label style="font-family:'Monaco'"><b>
-                                    <font size="4px" color="Black">Select Head Dept.</font>
-                                </b></label>
-                        </div>
-                    </div>
+                    <h2 style="font-family:'Monaco'">
+                        <b>
+                            <font size="4px" color="Black">Select Head Dept.</font>
+                        </b>
+                    </h2>
                     <!-- Select Head Dept. -->
 
                     <div class="form-group">
@@ -509,6 +564,8 @@ function manage_data(gru_id) {
                         </div>
                     </div>
                     <!-- Name Surname -->
+                </form>
+                <!-- form -->
             </div>
             <!-- modal-body -->
 
@@ -516,11 +573,10 @@ function manage_data(gru_id) {
                 <div class="btn-group pull-left">
                     <button type="button" class="btn btn-inverse" data-dismiss="modal">CANCEL</button>
                 </div>
-                <input type="submit" class="btn btn-success" value="SAVE">
+                <button type="submit" class="btn btn-success" id="btnedit<?php echo $row->gru_id; ?>"
+                onclick="return check_edit_skd('<?php echo $row->gru_id; ?>')">SAVE</button>
             </div>
             <!-- modal-footer -->
-            </form>
-            <!-- form-horizontal -->
         </div>
         <!-- modal-content -->
     </div>
