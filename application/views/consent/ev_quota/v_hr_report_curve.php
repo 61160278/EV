826a302ba0c4945b987d61ecb8f406a160ce339f
LapatradaPuttamongkol <70052206+LapatradaPuttamongkol@users.noreply.github.com>
2021-05-05 08:34:51 +0700

    Update v_hr_report_curve.php

--- application/views/consent/ev_quota/v_hr_report_curve.php
+++ application/views/consent/ev_quota/v_hr_report_curve.php
@@ -12,7 +12,7 @@
 $(document).ready(function() {
     check_quota_plan()
     check_quota_actual()
-    //document.getElementById("submit").disabled = true;
+    document.getElementById("submit").disabled = true;
 });
 function get_data() {
     var pos_sel = document.getElementById("pos_select").value; // get kay by id
@@ -109,14 +109,13 @@ function check_quota_actual() {
             sumQuotaActual += quotaActual;
             console.log(quotaActual + "=" + valueActual + "* 100 /" + parseInt(quota));
             actual += valueActual;
-
+            console.log(actual);
         }
-        
-        if (valueActual > parseInt(quota)) {
+        if (actual > parseInt(quota)) {
             $("#show_Actual").css("color", "red");
             add_alert();
             $("#submit").attr("disabled", true);
-        } else if (valueActual == parseInt(quota)) {
+        } else if (actual == parseInt(quota)) {
             $("#submit").attr("disabled", false);
             $("#show_Actual").css("color", "#000000");
         }
