be17d84e0b37db90d702b4f71da1c44d8aa27232
LapatradaPuttamongkol <70052206+LapatradaPuttamongkol@users.noreply.github.com>
2021-05-05 08:14:14 +0700

    Update v_hr_report_curve.php

--- application/views/consent/ev_quota/v_hr_report_curve.php
+++ application/views/consent/ev_quota/v_hr_report_curve.php
@@ -9,6 +9,11 @@
 */  
 ?>
 <script>
+$(document).ready(function() {
+    check_quota_plan()
+    check_quota_actual()
+    document.getElementById("submit").disabled = true;
+});
 function get_data() {
     var pos_sel = document.getElementById("pos_select").value; // get kay by id
     console.log(pos_sel);
@@ -61,12 +66,6 @@ function get_company() {
 }
 
 
-$(document).ready(function() {
-    check_quota_plan()
-    check_quota_actual()
-    document.getElementById("submit").disabled = true;
-});
-
 function check_quota_plan() {
 
     var check = "";
@@ -85,6 +84,7 @@ function check_quota_plan() {
 }
 
 function check_quota_actual() {
+    
     var check = "";
     var valueActual = 0;
     var actual = 0;
