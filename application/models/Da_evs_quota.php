6271c16da4cf13eaafe03f424f523ee357a16d72
piyasak61160185 <66992966+piyasak18013@users.noreply.github.com>
2021-05-12 08:34:49 +0700

    Update Da_evs_quota.php

--- application/models/Da_evs_quota.php
+++ application/models/Da_evs_quota.php
@@ -9,7 +9,7 @@
 * Da_evs_quota
 * Position Management
 * @author Piyasak Srijan
-* @update Date 2564-05-10
+* @update Date 2564-05-12
 */ 
 ?>
 <?php
@@ -41,10 +41,23 @@ class Da_evs_quota extends evs_model {
 	* @author Piyasak Srijan
 	* @Create Date 2564-05-10
 	*/
+
+	/*
+	* insert
+	* Insert Quota into database
+	* @input qut_id, qut_type,qut_pos,qut_grad_S,qut_grad_A,qut_grad_B,qut_grad_B_N,qut_grad_C,qut_grad_D
+	* @output -
+	* @author Piyasak Srijan
+	* @Update Date 2564-05-12
+	*/
 	function insert() {
-	 	$sql = "INSERT INTO evs_database.evs_quota (evs_quota.qut_id, evs_quota.qut_type, evs_quota.qut_pos, evs_quota.qut_date)VALUES(?, ?, ?, ?)";
+	 	$sql = "INSERT INTO evs_database.evs_quota (evs_quota.qut_id, evs_quota.qut_type, evs_quota.qut_pos, evs_quota.qut_date,
+		 		evs_quota.qut_grad_S, evs_quota.qut_grad_A, evs_quota.qut_grad_B, evs_quota.qut_grad_B_N, evs_quota.qut_grad_C,
+				evs_quota.qut_grad_D)
+		 		VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		 
-	 	$this->db->query($sql, array($this->qut_id, $this->qut_type, $this->qut_pos, $this->qut_date));
+	 	$this->db->query($sql, array($this->qut_id, $this->qut_type, $this->qut_pos, $this->qut_date, $this->qut_grad_S,
+						$this->qut_grad_A, $this->qut_grad_B, $this->qut_grad_B_N, $this->qut_grad_C, $this->qut_grad_D));
 	
 	 }
 
