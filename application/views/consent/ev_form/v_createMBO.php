19fdf1fb260cb3a7ca34cd15ee968848e638612c
Aofltyphoto <aofly2542@gmail.com>
2021-06-01 11:32:50 +0700

    Update v_createMBO.php

--- application/views/consent/ev_form/v_createMBO.php
+++ application/views/consent/ev_form/v_createMBO.php
@@ -1578,7 +1578,7 @@ function set_tap() {
                         <hr>
                         <div class="row">
                             <div class="col-md-12">
-                                <button class="btn btn-inverse"><i class="fa fa-mail-reply"></i> Back</button>
+                                <button class="btn btn-inverse" onclick="cancel_form()">BACK</button>
                             </div>
                             <!-- col-md-6 -->
 
@@ -1588,7 +1588,232 @@ function set_tap() {
                     </div>
                     <!-- form 2-1 -->
 
-                    <!-- *************************************************-->
+                    <!-- ************************************************************************************ -->
+
+
+                    <div class="tab-pane" id="GCM">
+                        <br>
+                        <?php foreach($emp_info->result() as $row){?>
+
+                        <input type="text" id="pos_id_acm" value="<?php echo $row->Position_ID; ?>" hidden>
+
+                        <div class="row">
+                            <div class="col-md-2">
+                                <label class="control-label"><strong>
+                                        <font size="3px">Employee ID : </font>
+                                    </strong></label>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <p id="emp_id"><?php echo $row->Emp_ID; ?></p>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <label class="control-label"><strong>
+                                        <font size="3px">Name : </font>
+                                    </strong></label>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <p id="emp_name"><?php echo $row->Empname_eng; ?></p>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <label class="control-label"><strong>
+                                        <font size="3px">Surname : </font>
+                                    </strong></label>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <p id="emp_lname"><?php echo $row->Empsurname_eng; ?></p>
+                            </div>
+                            <!-- col-md-2 -->
+                        </div>
+                        <!-- row -->
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-2">
+                                <label class="control-label"><strong>
+                                        <font size="3px">Section Code : </font>
+                                    </strong></label>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <p id="emp_sec"><?php echo $row->Sectioncode_ID; ?></p>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <label class="control-label"><strong>
+                                        <font size="3px">Department : </font>
+                                    </strong></label>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <p id="emp_dep"><?php echo $row->Department; ?></p>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <label class="control-label"><strong>
+                                        <font size="3px">Position : </font>
+                                    </strong></label>
+                            </div>
+                            <!-- col-md-2 -->
+                            <div class="col-md-2">
+                                <p id="emp_pos"><?php echo $row->Position_name; ?></p>
+                            </div>
+                            <!-- col-md-2 -->
+                        </div>
+                        <!-- row -->
+                        <?php }; ?>
+                        <!-- show infomation employee -->
+                        <hr>
+
+                        <table class="table table-bordered table-striped m-n" id="mbo">
+                            <thead id="headmbo">
+                                <tr>
+                                    <th rowspan="2">
+                                        <center> No.</center>
+                                    </th>
+                                    <th rowspan="2">
+                                        <center>Competency</center>
+                                    </th>
+                                    <th rowspan="2">
+                                        <center>Key component</center>
+                                    </th>
+                                    <th rowspan="2">
+                                        <center>Expected Behavior</center>
+                                    </th>
+                                    <th rowspan="2" width="6%">
+                                        <center>Weight</center>
+                                    </th>
+                                    <th colspan="2">
+                                        <center>Evaluation</center>
+                                    </th>
+
+                                </tr>
+                                <tr>
+                                    <th width="25%">
+                                        <center>Result</center>
+                                    </th>
+                                    <th width="15%">
+                                        <center>Score AxB</center>
+                                    </th>
+
+                                </tr>
+                            </thead>
+                            <!-- thead -->
+                            <tbody id="dis_color">
+                                <?php  
+                                    $index_acm = 1;
+                                    $temp_keycomponent = "";
+                                    $temp_expected = "";
+                                    $sum_max_rating = 0;
+                                    // start foreach
+                                    foreach($info_ability_form->result() as $row){
+                                ?>
+                                <tr>
+                                    <td id="dis_color">
+                                        <center><?php echo $index_acm++; ?></center>
+                                    </td>
+                                    <td id="dis_color">
+                                        <?php echo $row->cpn_competency_detail_en . "<br><font color='blue'>" . $row->cpn_competency_detail_th ."</font>"; ?>
+                                    </td>
+                                    <!-- show competency  -->
+                                    <td id="dis_color">
+                                        <?php foreach($info_expected->result() as $row_ept){ 
+                                            if($row->sfa_cpn_id == $row_ept->kcp_cpn_id && $temp_keycomponent != $row_ept->kcp_key_component_detail_en){
+                                                $temp_keycomponent = $row_ept->kcp_key_component_detail_en;?>
+                                        <?php echo $row_ept->kcp_key_component_detail_en . "<br><font color='blue'>" . $row_ept->kcp_key_component_detail_th ."</font>"; ?>
+                                        <?php }
+                                            // if
+                                            }
+                                            // foreach ?>
+                                    </td>
+                                    <!-- show key component  -->
+                                    <td id="dis_color">
+                                        <?php foreach($info_expected->result() as $row_ept){ 
+                                            if($row->sfa_cpn_id == $row_ept->kcp_cpn_id && $temp_expected != $row_ept->ept_expected_detail_en && $row_ept->ept_pos_id == $info_pos_id){
+                                                $temp_expected = $row_ept->ept_expected_detail_en;?>
+                                        <?php echo $row_ept->ept_expected_detail_en . "<br><font color='blue'>" . $row_ept->ept_expected_detail_th ."</font><hr>"; ?>
+                                        <?php }
+                                        // if
+                                        }
+                                        // foreach ?>
+                                    </td>
+                                    <!-- show expected  -->
+                                    <td id="dis_color">
+                                        <center><?php echo $row->sfa_weight; ?></center>
+                                    </td>
+                                    <!-- show weight  -->
+                                    <td id="dis_color" width="5%">
+                                        <center>
+                                            <div class="col-md-12">
+                                                <form action="">
+                                                    <input type="radio" name="result" value="1" Disabled Unchecked>
+                                                    <label for="1">&nbsp; 1</label>
+                                                    &nbsp;&nbsp;
+                                                    <input type="radio" name="result" value="2" Disabled Unchecked>
+                                                    <label for="2">&nbsp; 2</label>
+                                                    &nbsp;&nbsp;
+                                                    <input type="radio" name="result" value="3" Disabled Unchecked>
+                                                    <label for="3">&nbsp; 3</label>
+                                                    &nbsp;&nbsp;
+                                                    <input type="radio" name="result" value="4" Disabled Unchecked>
+                                                    <label for="4">&nbsp; 4</label>
+                                                    &nbsp;&nbsp;
+                                                    <input type="radio" name="result" value="5" Disabled Unchecked>
+                                                    <label for="5">&nbsp; 5</label>
+                                                    &nbsp;&nbsp;
+                                                </form>
+                                            </div>
+                                            <!-- col-12 -->
+                                        </center>
+                                    </td>
+                                    <td id="dis_color" width="2%"></td>
+                                </tr>
+
+                                <?php
+                                    }
+                                    // end foreach
+                                ?>
+                            </tbody>
+                            <!-- tbody -->
+                            <!-- tbody -->
+
+                            <tfoot>
+                                <tr height="5%" id="dis_color">
+                                    <td colspan="4">
+                                        <center> Total Weight</center>
+                                    </td>
+                                    <td>
+                                        <center> 100</center>
+                                    </td>
+                                    <td>
+                                        <center> Total Result</center>
+                                    </td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                            </tfoot>
+                            <!-- tfoot -->
+
+                        </table>
+                        <!-- table -->
+
+                        <br>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button class="btn btn-inverse" onclick="cancel_form()">BACK</button>
+                            </div>
+                            <!-- col-md-6 -->
+
+                        </div>
+                        <!-- row -->
+
+                    </div>
+                    <!-- form 2-1 -->
+
+                    <!-- ************************************************************************************ -->
 
                 </div>
                 <!-- tab-content -->
