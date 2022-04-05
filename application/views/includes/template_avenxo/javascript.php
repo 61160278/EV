4cb815f6689b104ce35919760a51482e2799742a
61160278 <71628924+61160278@users.noreply.github.com>
2022-04-05 17:04:15 +0700

    Update javascript.php

--- application/views/includes/template_avenxo/javascript.php
+++ application/views/includes/template_avenxo/javascript.php
@@ -101,9 +101,6 @@
 
 <!-- Load datatable scripts-->
 <script type="text/javascript" src="<?php echo base_url();?>DataTables/datatables.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script> 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.html5.min.js"></script>
 <!-- End datatablel scripts-->
 
 <!-- End loading page level scripts-->
